5a3c1178ec75d54930abf68ae0e29ddd6e8f9a0b
Bogush-Dima <bogusd66@gmail.com>
2021-04-26 16:14:52 +0300

    add buttons for form to fill structure

--- src/components/FormToFill/component.tsx
+++ src/components/FormToFill/component.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import {Grid, TextField} from "@material-ui/core";
+import {Button, Grid, TextField} from "@material-ui/core";
+import {useStyles} from "./styles";
 
 export const FormToFill: React.FC = () => {
+    const classes = useStyles()
+
     return (
-        <form>
-            <TextField label='Number'/>
-            <TextField label='Text'/>
-            <Grid container className='wrapper'>
-                <TextField label='Date'/>
-                <TextField label='Code'/>
+        <>
+            <form className={classes.form}>
+                <TextField label='Number'/>
+                <TextField label='Name'/>
+                <Grid container className='wrapper'>
+                    <TextField label='Date'/>
+                    <TextField label='Code'/>
+                </Grid>
+            </form>
+            <Grid container justify='center' alignItems='center'>
+                <Button variant="contained" color="primary">Submit</Button>
+                <Button variant="contained">Reset</Button>
             </Grid>
-        </form>
+        </>
     )
 }
